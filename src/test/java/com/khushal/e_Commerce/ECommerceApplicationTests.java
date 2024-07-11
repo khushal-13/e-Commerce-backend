9fbbbe5c33b6ba417bff60b0d34a64c5715146f0
khushal-13 <khushaljogi7@gmail.com>
2024-07-11 22:16:28 +0530

    First commit

---
+++ src/test/java/com/khushal/e_Commerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khushal.e_Commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
